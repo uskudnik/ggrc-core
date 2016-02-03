07fcbbce2ded8b6758a4eaee23546bb2d612e85c
Urban Skudnik <urban.skudnik@gmail.com>
2016-02-03 20:37:22 +0100

    full feature working implementation

--- src/ggrc/assets/javascripts/components/mapped_tree_view.js
+++ src/ggrc/assets/javascripts/components/mapped_tree_view.js
@@ -11,59 +11,96 @@
     tag: "reuse-objects",
     scope: {
       parentInstance: null,
-      reusedObjects: [],
-      _create_relationship: function(source, destination) {
-        if (!destination) {
+      reusedObjects: new can.List(),
+      create_relationship: function(destination) {
+        console.log("_create_relationship", this, destination);
+        var source;
+        var dest;
+
+        if (!this.scope || !destination) {
           return $.Deferred().resolve();
         }
 
+        source = this.scope.attr("parentInstance");
+        dest = CMS.Models.get_instance({
+          id: destination.id,
+          type: can.spaceCamelCase(destination.type).replace(/ /g, '')
+        });
+
         return new CMS.Models.Relationship({
           source: source.stub(),
-          destination: destination,
+          destination: dest,
           context: source.context,
+        }).save();
+      },
+      create_evidence_relationship: function(destination) {
+        console.log("create_evidence_relationship", destination);
+        var source;
+        var dest;
+
+        if (!this.scope || !destination) {
+          return $.Deferred().resolve();
+        }
+
+        source = this.scope.attr("parentInstance");
+        dest = CMS.Models.get_instance({
+          id: destination.id,
+          type: can.spaceCamelCase(destination.type).replace(/ /g, '')
+        });
+
+        //console.log("dest: ", dest);
+
+        return new CMS.Models.ObjectDocument({
+          context : source.context,
+          documentable : source,
+          document : dest,
         }).save();
       },
     },
     events: {
       init: function(el, ev) {
+
         //debugger;
       },
       "inserted": function(el, ev) {
-        console.log("init", el, ev);
+        console.log("inserted", el, ev);
+        console.log("parent instance: ", this.scope.parentInstance);
+        console.log("parent instance: ", this.options.scope.parentInstance);
         //debugger;
         // kje bi naj sicer prisel tale?
         //this.scope.attr("parent_instance", GGRC.page_instance());
       },
+      "[reusable=true] input[type=checkbox] change": function (el, ev) {
+        var reused = this.scope.attr("reusedObjects");
+        var object = el.parent();
+        console.log("method: ", object.parents("[reusable=true]").attr("reuse-method"));
+        //debugger;
+        var key = {
+          type: object.attr("data-object-type"),
+          id: object.attr("data-object-id"),
+          method: object.parents("[reusable=true]").attr("reuse-method")
+        };
+        var index = _.findIndex(reused, key);
+        if (index >= 0) {
+          reused.splice(index, 1);
+          return;
+        }
+        reused.push(key);
+      },
       ".js-trigger-reuse click": function(el, ev) {
-        console.log("blablabla reuse");
-        var umbrella = el.parents("reuse-objects");
-        var checked = umbrella.find(
-          "[reusable] li input[type=checkbox]:checked");
-        var related_dfds = can.map(checked, function (c) {
-          var elem = $(c);
-          var type = elem.attr("data-object-type");
-          var id = elem.attr("data-object-id");
-          //  //console.log("gonna link it with: ", type, id);
-          console.log("this: ", this);
-          console.log("this stub: ", this.stub());
-          console.log("this context stub: ", this.context.stub());
-          //return type+id;
-          var dest = CMS.Models.get_instance({
-            id: id,
-            type: can.spaceCamelCase(type).replace(/ /g, '')
-          });
-          console.log("dest: ", dest);
+        console.log("clicked reuse");
 
-          return new CMS.Models.Relationship({
-                source: this.stub(),
-                destination: dest.stub(),
-                context: this.context.stub()
-              }
-            ).save();
-        }.bind(this.scope.attr("parent_instance")));
+        var reused = this.scope.attr("reusedObjects");
+        var related_dfds = can.map(reused, function (object) {
+          console.log("this: ", this);
+          console.log("o: ", object);
+          var executer = this.scope[object.method].bind(this);
+          console.log(executer);
+
+          return executer(object);
+        }.bind(this));
+        //}.bind(this.scope.attr("parentInstance")));
         GGRC.delay_leaving_page_until($.when.apply($, related_dfds));
-        console.log("PI: ", this.scope.attr("parent_instance"));
-        console.log("checked: ", related_dfds);
       },
     }
   });
@@ -90,22 +127,30 @@
             mapping.destroy();
           });
         });
-      },
-      "input change": function (el, ev) {
-        console.log("list: ", this.scope.list);
-        console.log("bla");
       }
     }
   });
 
-  //can.Component.extend({
-  //  tag: "mapped-object",
-  //  events: {
-  //    "inserted": function(el, ev) {
-  //      if (el.parents("reuse-objects")) {
-  //        el.find("li").append('<input type="checkbox" name="reuseable_objects" data-object-type='+el.attr("data-object-type")+' data-object-id='+el.attr("data-object-id")+' />');
-  //      }
-  //    }
-  //  }
-  //});
+  can.Component.extend({
+    tag: "reusable-object",
+    template: "<content></content>",
+    scope: {
+      is_reusable: null
+    },
+    events: {
+      "inserted": function(el, ev) {
+        if (el.parents("[reusable=true]").length === 1) {
+          this.scope.attr("is_reusable", true);
+        }
+      }
+    },
+    helpers: {
+      if_reusable: function(options) {
+        if (this.attr("is_reusable") === true) {
+          return options.fn();
+        }
+        return options.inverse();
+      }
+    }
+  });
 })(window.can, window.can.$);
