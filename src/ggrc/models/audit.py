dc814edd766a7bc01265162d60264f05c8ccd9b2
Urban Skudnik <urban.skudnik@gmail.com>
2016-08-09 18:02:34 +0200

    Make Audit snapshotable

--- src/ggrc/models/audit.py
+++ src/ggrc/models/audit.py
@@ -19,8 +19,10 @@ from ggrc.models.reflection import PublishOnly
 from ggrc.models.program import Program
 from ggrc.models.person import Person
 
+from ggrc.models.snapshot import Snapshotable
 
-class Audit(clonable.Clonable,
+
+class Audit(Snapshotable, clonable.Clonable,
             CustomAttributable, Personable, HasOwnContext, Relatable,
             Timeboxed, Noted, Described, Hyperlinked, WithContact, Titled,
             Slugged, db.Model):
