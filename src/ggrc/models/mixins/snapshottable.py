af1bf94537fa715b7e80a6efb1f0ba9e2fbc56fa
Urban Skudnik <urban.skudnik@gmail.com>
2016-12-15 16:46:40 +0100

    Add SnapshottableChild mixin
    Add a mixin that will return all snapshots of base first class
    object

--- src/ggrc/models/mixins/snapshottable.py
+++ src/ggrc/models/mixins/snapshottable.py
@@ -3,8 +3,9 @@
 
 """Module for Snapshot object"""
 
-from sqlalchemy.ext.declarative import declared_attr
+from sqlalchemy import and_
 from sqlalchemy import orm
+from sqlalchemy.ext.declarative import declared_attr
 
 
 from ggrc import db
@@ -40,3 +41,26 @@ class SnapshottableParent(object):
             "Snapshot_complete"
         ),
     )
+
+
+class SnapshottableChild(object):
+  """Provide `related_snapshots` on all snapshottable objects."""
+  @declared_attr
+  def related_snapshots(cls):  # pylint: disable=no-self-argument
+    """Return all snapshots for some snapshottable objects"""
+    import ggrc
+
+    return db.relationship(
+        lambda: ggrc.models.Snapshot,
+        primaryjoin=lambda: and_(
+            orm.remote(ggrc.models.Snapshot.child_id) == cls.id,
+            orm.remote(ggrc.models.Snapshot.child_type) == cls.__name__
+        ),
+        foreign_keys="Snapshot.child_id,Snapshot.child_type",
+        backref="{0}_child".format(cls.__name__),
+    )
+
+  @classmethod
+  def eager_query(cls):
+    query = super(SnapshottableChild, cls).eager_query()
+    return query.options(orm.subqueryload("related_snapshots"))
